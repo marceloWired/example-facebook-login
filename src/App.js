81523dc74df0d9bc85355d55bc123e7236cc99d1
marceloWired <marcelo.santosdev1@gmail.com>
2024-03-06 15:47:18 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import FacebookLogin from '@greatsumini/react-facebook-login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FacebookLogin
+        appId="appId"
+        scope='instagram_basic,instagram_content_publish,pages_show_list,business_management,instagram_manage_insights'
+        onSuccess={(response) => {
+          console.log('Login Success!', response);
+        }}
+        onFail={(error) => {
+          console.log('Login Failed!', error);
+        }}
+        onProfileSuccess={(response) => {
+          console.log('Get Profile Success!', response);
+        }}
+    />
     </div>
   );
 }
